f3f816c21830cffcb97397ac3f92133f8d787542
Oleh Yurchenko <olegurcenko4@gmail.com>
2023-07-09 21:09:20 +0200

    added links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,19 +10,19 @@ export const Footer = (props) => {
         <div className={styles.footer}>
             <ul className={styles.footerList}>
                 <li className={styles.footerListElem}>
-                    <a href="#"><img src={gitLogo} alt="" />
+                    <a href="https://github.com/olegurcenko"><img src={gitLogo} alt="" />
                     </a>
                 </li>
                 <li className={styles.footerListElem}>
-                    <a href="#"><img src={instLogo} alt="" />
+                    <a href="https://www.instagram.com/abuser_79/"><img src={instLogo} alt="" />
                     </a>
                 </li>
                 <li className={styles.footerListElem}>
-                    <a href="#"><img src={tgLogo} alt="" />
+                    <a href="https://t.me/user_3217"><img src={tgLogo} alt="" />
                     </a>
                 </li>
                 <li className={styles.footerListElem}>
-                    <a href="#"><img src={gmailLogo} alt="" />
+                    <a href="mailto:https://mail.google.com/mail/u/0/?pli=1#inbox"><img src={gmailLogo} alt="" />
                     </a>
                 </li>
             </ul>
